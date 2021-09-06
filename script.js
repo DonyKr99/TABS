037ce72d1a0cbf1ee8e6ef53144246658ebee546
DonyKr99 <87666573+DonyKr99@users.noreply.github.com>
2021-09-06 08:18:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var tab = document.getElementById("pirmas");
+      tab.style.display = "block";
+      function changeHeader(target, name){
+        var tabs = document.getElementsByClassName("tab");
+        var buttons = document.getElementsByTagName("button");
+        for (var i = 0; i < tabs.length; i++){
+          tabs[i].style.display = "none";
+          buttons[i].classList.remove("active");
+        }
+        var tab = document.getElementById(name);
+        tab.style.display = "block";
+        target.currentTarget.classList.add("active");
+        console.log(name);
+      }
